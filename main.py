8c59d9f5258fe9b2c36eab5e0c9414ccb39d8bf2
harishkumargithub <120339483+harishkumargithub@users.noreply.github.com>
2023-02-14 14:05:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import glob
 import json
 import os
 import pandas as pd
-#import streamlit as st
+import streamlit as st
 from sqlalchemy import create_engine
 
 
@@ -22,31 +22,59 @@ def aggregated_transaction_push():
                     df.append({'Name': f, 'Count': j['count'], 'Amount': j['amount']})
 
     df = pd.DataFrame(df)
-    print(df)
     return (df)
 
 
-def push_to_sql(df):
+def aggregated__transaction_push_to_sql(df):
     engine = create_engine(
         "mysql+mysqldb://root:asdfghjkl$12@localhost:3306/mydb")
     df.to_sql(name='aggregatedtransaction', con=engine, if_exists='append', chunksize=1000, index=False)
 
 
-#opt = str(st.sidebar.selectbox(label='select', options=[' ', 'aggregated', 'map']))
-#opt2 = str(st.sidebar.selectbox(label='select', options=[' ', 'transaction', 'user']))
-# elif opt == 'aggregated' and opt2 == 'user':
-#     os.chdir('PhonePePulseData/data/aggregated/user')
-# elif opt == 'map' and opt2 == 'transaction':
-#     os.chdir('PhonePePulseData/data/map/transaction')
-# elif opt == 'map' and opt2 == 'user':
-#     os.chdir('PhonePePulseData/data/map/user')
-# elif opt == 'top' and opt2 == 'transaction':
-#     os.chdir('PhonePePulseData/data/top/transaction')
-# elif opt == 'map' and opt2 == 'user':
-#     os.chdir('PhonePePulseData/data/top/user')
+def map_transaction_push_to_sql(df):
+    engine = create_engine(
+        "mysql+mysqldb://root:asdfghjkl$12@localhost:3306/mydb")
+    df.to_sql(name='map_transaction', con=engine, if_exists='append', chunksize=1000, index=False)
 
-#if st.button(label='Upload'):
-df = aggregated_transaction_push()
-push_to_sql(df)
-    #st.write(df.head())
 
+def map_transaction_push():
+    os.chdir('PhonePePulseData/data/Transaction/map')
+    f = ''
+    df = []
+    for filename in glob.glob('*.json'):
+        with open(filename) as f:
+            g = json.loads(f.read())
+            e = pd.json_normalize(g)
+        e.drop(['success', 'code'], axis=1, inplace=True)
+        for each in e['data.hoverDataList']:
+            for i in each:
+                f = i['name']
+                for j in i['metric']:
+                    df.append({'Name': f, 'Count': j['count'], 'Amount': j['amount']})
+
+    df = pd.DataFrame(df)
+    return (df)
+
+
+opt = str(st.sidebar.selectbox(label='select', options=[' ', 'aggregated', 'map']))
+opt2 = str(st.sidebar.selectbox(label='select', options=[' ', 'transaction', 'user']))
+upload = st.sidebar.button(label='Upload')
+if opt == 'aggregated' and opt2 == 'transaction' and upload:
+    df = aggregated_transaction_push()
+    st.write(df.head())
+    aggregated__transaction_push_to_sql()
+    st.write('SQL upload successful')
+
+elif opt == 'aggregated' and opt2 == 'user':
+    os.chdir('PhonePePulseData/data/aggregated/user')
+elif opt == 'map' and opt2 == 'transaction' and upload:
+    df = map_transaction_push()
+    st.write('Push successful')
+    map_transaction_push_to_sql(df)
+    st.write('SQL upload successful')
+elif opt == 'map' and opt2 == 'user' and upload:
+    pass
+elif opt == 'top' and opt2 == 'transaction':
+    os.chdir('PhonePePulseData/data/top/transaction')
+elif opt == 'map' and opt2 == 'user':
+    os.chdir('PhonePePulseData/data/top/user')
