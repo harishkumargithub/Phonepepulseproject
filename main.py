4e9682a6a60a9a149719eb7ba30093251401fbb0
harishkumargithub <120339483+harishkumargithub@users.noreply.github.com>
2023-02-16 13:26:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import streamlit as st
 from sqlalchemy import create_engine
+from geopy import Nominatim
 
 
 def aggregated_transaction_push():
@@ -31,12 +32,6 @@ def aggregated__transaction_push_to_sql(df):
     df.to_sql(name='aggregatedtransaction', con=engine, if_exists='append', chunksize=1000, index=False)
 
 
-def map_transaction_push_to_sql(df):
-    engine = create_engine(
-        "mysql+mysqldb://root:asdfghjkl$12@localhost:3306/mydb")
-    df.to_sql(name='map_transaction', con=engine, if_exists='append', chunksize=1000, index=False)
-
-
 def map_transaction_push():
     os.chdir('PhonePePulseData/data/Transaction/map')
     f = ''
@@ -56,22 +51,68 @@ def map_transaction_push():
     return (df)
 
 
+def map_transaction_on_map(d):
+    a = d['Name'].unique()
+    map = []
+    for each in a:
+        geolocator = Nominatim(user_agent='MyApp')
+        location = geolocator.geocode(each)
+        map.append({'lat': location.latitude, 'lon': location.longitude})
+    df_map = pd.DataFrame(map)
+    return df_map
+
+
+def map_transaction_push_to_sql(df):
+    engine = create_engine(
+        "mysql+mysqldb://root:asdfghjkl$12@localhost:3306/mydb")
+    df.to_sql(name='map_transaction', con=engine, if_exists='append', chunksize=1000, index=False)
+
+
+def aggregated_user_push():
+    os.chdir('PhonePePulseData/data/User/Aggregated')
+    df = []
+    for filename in glob.glob('*.json'):
+        with open(filename) as f:
+            g = json.loads(f.read())
+            e = pd.json_normalize(g)
+        e.drop(['success', 'code'], axis=1, inplace=True)
+        for each in e['data.usersByDevice']:
+            if each is not None:
+                for j in each:
+                    df.append({'Brand': j['brand'], 'Count': j['count'], 'Percentage': j['percentage']})
+        df2 = pd.DataFrame(df)
+        return df2
+
+
+def aggregated_user_push_to_sql(df):
+    engine = create_engine(
+        "mysql+mysqldb://root:asdfghjkl$12@localhost:3306/mydb")
+    df.to_sql(name='aggregated_user', con=engine, if_exists='append', chunksize=1000, index=False)
+
+
 opt = str(st.sidebar.selectbox(label='select', options=[' ', 'aggregated', 'map']))
 opt2 = str(st.sidebar.selectbox(label='select', options=[' ', 'transaction', 'user']))
 upload = st.sidebar.button(label='Upload')
 if opt == 'aggregated' and opt2 == 'transaction' and upload:
     df = aggregated_transaction_push()
     st.write(df.head())
-    aggregated__transaction_push_to_sql()
+    aggregated__transaction_push_to_sql(df)
     st.write('SQL upload successful')
 
-elif opt == 'aggregated' and opt2 == 'user':
-    os.chdir('PhonePePulseData/data/aggregated/user')
+elif opt == 'aggregated' and opt2 == 'user' and upload:
+    d = aggregated_user_push()
+    st.write('Push successful')
+    aggregated_user_push_to_sql(d)
+    st.write('SQL upload successful')
+
 elif opt == 'map' and opt2 == 'transaction' and upload:
     df = map_transaction_push()
     st.write('Push successful')
     map_transaction_push_to_sql(df)
     st.write('SQL upload successful')
+    df_map = map_transaction_on_map(df)
+    st.map(df_map)
+
 elif opt == 'map' and opt2 == 'user' and upload:
     pass
 elif opt == 'top' and opt2 == 'transaction':
